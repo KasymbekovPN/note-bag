f07e31f5cc1182cb4af0a00eef645cb0484406b5
KasymbekovPN <kasymbekovPN@yandex.ru>
2021-09-20 22:24:00 +0300

    NPBot refactoring

--- src/main/java/ru/kpn/bot/NPBot.java
+++ src/main/java/ru/kpn/bot/NPBot.java
@@ -1,39 +1,41 @@
 package ru.kpn.bot;
 
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.kpn.bot.publisher.Publisher;
 import ru.kpn.calculator.extractor.ExtractorCalculatorFactory;
-import ru.kpn.i18n.builder.MessageBuilderFactory;
 import ru.kpn.subscriber.Subscriber;
 
 import java.util.function.Consumer;
 
+@Service
 public class NPBot extends TelegramWebhookBot implements Publisher<Update, BotApiMethod<?>>, Consumer<Update> {
 
     private final String botPath;
     private final String botUserName;
     private final String botToken;
     private final ExtractorCalculatorFactory<Update, BotApiMethod<?>> calculatorFactory;
-    private final MessageBuilderFactory messageBuilderFactory;
 
     private Subscriber<Update, BotApiMethod<?>> subscriber;
 
+    @Autowired
     public NPBot(DefaultBotOptions options,
-                 String botPath,
-                 String botUserName,
-                 String botToken,
-                 ExtractorCalculatorFactory<Update, BotApiMethod<?>> calculatorFactory,
-                 MessageBuilderFactory messageBuilderFactory) {
+                 @Value("${bot.path}") String botPath,
+                 @Qualifier("botUserName") String botUserName,
+                 @Qualifier("botToken") String botToken,
+                 ExtractorCalculatorFactory<Update, BotApiMethod<?>> calculatorFactory) {
         super(options);
         this.botPath = botPath;
         this.botUserName = botUserName;
         this.botToken = botToken;
         this.calculatorFactory = calculatorFactory;
-        this.messageBuilderFactory = messageBuilderFactory;
     }
 
     // TODO: 04.09.2021 test it
--- src/main/java/ru/kpn/config/bot/NPBotConfig.java
+++ src/main/java/ru/kpn/config/bot/NPBotConfig.java
@@ -5,45 +5,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import ru.kpn.bot.NPBot;
-import ru.kpn.calculator.extractor.ExtractorCalculatorFactory;
 import ru.kpn.decryptor.Decryptor;
-import ru.kpn.calculator.extractor.ExtractorCalculatorFactoryImpl;
-import ru.kpn.i18n.I18n;
-import ru.kpn.i18n.builder.MessageBuilderFactory;
 
 @Configuration
 public class NPBotConfig {
 
-    @Autowired
-    private MessageBuilderFactory messageBuilderFactory;
-
-    @Value("${bot.path}")
-    private String botPath;
     @Value("${bot.name}")
     private String botUserName;
     @Value("${bot.token}")
     private String botToken;
 
     @Autowired
-    private DefaultBotOptions botOptions;
-
-    @Autowired
     private Decryptor decryptor;
 
-    @Autowired
-    private ExtractorCalculatorFactory<Update, BotApiMethod<?>> extractorCalculatorFactory;
+    @Bean
+    public DefaultBotOptions botOptions(){
+        return new DefaultBotOptions();
+    }
+    
+    @Bean
+    public String botToken(){
+        return decryptor.decrypt(botToken);
+    }
 
     @Bean
-    public NPBot npBot(){
-        return new NPBot(
-                botOptions,
-                botPath,
-                decryptor.decrypt(botUserName),
-                decryptor.decrypt(botToken),
-                extractorCalculatorFactory,
-                messageBuilderFactory);
+    public String botUserName(){
+        return decryptor.decrypt(botUserName);
     }
 }
