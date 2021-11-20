aa6bebd8c5c59e8647184a1e50f88c7279ae853b
KasymbekovPN <kasymbekovPN@yandex.ru>
2021-11-20 17:17:56 +0300

    refactoring

--- src/main/java/ru/kpn/objectExtraction/builder/MatcherBuilder.java
+++ src/main/java/ru/kpn/objectExtraction/builder/MatcherBuilder.java
@@ -1,21 +1,16 @@
 package ru.kpn.objectExtraction.builder;
 
-import lombok.RequiredArgsConstructor;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.kpn.objectExtraction.datum.MatcherDatum;
 import ru.kpn.objectExtraction.factory.MatcherFactory;
 import ru.kpn.objectExtraction.factory.ObjectFactory;
-import ru.kpn.objectExtraction.result.Result;
-import ru.kpn.objectExtraction.result.ResultImpl;
 import ru.kpn.rawMessage.RawMessageFactory;
 
 import java.util.EnumMap;
 import java.util.Optional;
 import java.util.function.Function;
 
-// TODO: 18.11.2021 need base class
-@RequiredArgsConstructor
-public class MatcherBuilder implements Builder<MatcherDatum, Function<Update, Boolean>> {
+public class MatcherBuilder extends BaseBuilder<MatcherDatum, Function<Update, Boolean>, MatcherFactory.Type> {
 
     private static final EnumMap<MatcherFactory.Type, Function<MatcherDatum, Optional<Object[]>>> CHECK_AND_PREPARE = new EnumMap<>(MatcherFactory.Type.class){{
         put(MatcherFactory.Type.CONSTANT, MatcherBuilder::checkAndPrepareConstant);
@@ -23,26 +18,8 @@ public class MatcherBuilder implements Builder<MatcherDatum, Function<Update, Bo
         put(MatcherFactory.Type.MULTI_REGEX, MatcherBuilder::checkAndPrepareMultiRegex);
     }};
 
-    private final ObjectFactory<MatcherFactory.Type, Function<Update, Boolean>> factory;
-    private final RawMessageFactory<String> rawMessageFactory;
-
-    private Result<Function<Update, Boolean>> result;
-    private String key;
-    private MatcherDatum datum;
-    private MatcherFactory.Type type;
-    private Object[] args;
-
-    @Override
-    public Builder<MatcherDatum, Function<Update, Boolean>> start(String key) {
-        this.result = new ResultImpl<>();
-        this.key = key;
-        return this;
-    }
-
-    @Override
-    public Builder<MatcherDatum, Function<Update, Boolean>> datum(MatcherDatum datum) {
-        this.datum = datum;
-        return this;
+    public MatcherBuilder(ObjectFactory<MatcherFactory.Type, Function<Update, Boolean>> objectFactory, RawMessageFactory<String> messageFactory) {
+        super(objectFactory, messageFactory);
     }
 
     @Override
@@ -53,16 +30,9 @@ public class MatcherBuilder implements Builder<MatcherDatum, Function<Update, Bo
         return this;
     }
 
-    @Override
-    public Result<Function<Update, Boolean>> build() {
-        Result<Function<Update, Boolean>> result = create();
-        reset();
-        return result;
-    }
-
     private void checkMatcherDataExistence() {
         if (result.getSuccess() && datum == null){
-            result.makeFailure(rawMessageFactory.create("data.notExist.forSth").add(key));
+            result.makeFailure(messageFactory.create("data.notExist.forSth").add(key));
         }
     }
 
@@ -73,10 +43,10 @@ public class MatcherBuilder implements Builder<MatcherDatum, Function<Update, Bo
                 try{
                     type = MatcherFactory.Type.valueOf(datumType);
                 } catch (IllegalArgumentException ex){
-                    result.makeFailure(rawMessageFactory.create("type.invalid.where").add(datumType).add(key));
+                    result.makeFailure(messageFactory.create("type.invalid.where").add(datumType).add(key));
                 }
             } else {
-                result.makeFailure(rawMessageFactory.create("type.isNull").add(key));
+                result.makeFailure(messageFactory.create("type.isNull").add(key));
             }
         }
     }
@@ -87,23 +57,9 @@ public class MatcherBuilder implements Builder<MatcherDatum, Function<Update, Bo
             if (maybeArgs.isPresent()){
                 this.args = maybeArgs.get();
             } else {
-                result.makeFailure(rawMessageFactory.create("arguments.invalid.forSth").add(key));
+                result.makeFailure(messageFactory.create("arguments.invalid.forSth").add(key));
             }
         }
-    }
-
-    private void reset() {
-        key = null;
-        datum = null;
-        type = null;
-        args = null;
-    }
-
-    private Result<Function<Update, Boolean>> create() {
-        if (result.getSuccess()){
-            result.setValue(factory.create(type, args));
-        }
-        return result;
     }
 
     private static Optional<Object[]> checkAndPrepareConstant(MatcherDatum datum){
